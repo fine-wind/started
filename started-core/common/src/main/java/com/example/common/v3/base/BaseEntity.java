e46d4704bcc753107725f8fd90d0fd13947c8e33
xing <xingii@outlook.com>
2024-03-17 18:53:22 +0800

    配置项

---
+++ started-core/common/src/main/java/com/example/common/v3/base/BaseEntity.java
@@ -0,0 +1,41 @@
+package com.example.common.v3.base;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 基础实体类，所有实体都需要继承
+ */
+@Data
+public class BaseEntity implements Serializable {
+    /**
+     * id
+     */
+    @TableId
+    private String id;
+    /**
+     * 创建者
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private String creator;
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createDate;
+    /**
+     * 更新者
+     */
+    @TableField(fill = FieldFill.UPDATE)
+    private Long updater;
+    /**
+     * 更新时间
+     */
+    @TableField(fill = FieldFill.UPDATE)
+    private Date updateDate;
+}
